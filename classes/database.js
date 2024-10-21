825d1c4255ff46779f193bf2547349bf6ab0deaf
Collin Van der Vorst <collin@littlemissrobot.com>
2024-10-21 15:03:45 +0200

    using env instead of hardcoded

--- classes/database.js
+++ classes/database.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 class Database {
     async connect() {
         const connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'root',
-            database: 'EurosongDB',
-            port: 8889
+            host: process.env.db_host,
+            user: process.env.db_user,
+            password: process.env.db_pass,
+            database: process.env.db_name,
+            port: process.env.db_port
         });
 
         return connection;
